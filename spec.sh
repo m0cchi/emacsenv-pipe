afb99f10f50b5118ae52e946fe389cc691e99fbf
m0cchi <m0cchi@users.noreply.github.com>
2016-10-15 15:07:51 +0900

    Update spec.sh

--- spec.sh
+++ spec.sh
@@ -1,6 +1,6 @@
-plugin_file="$(cd $(dirname $0); pwd)/plugins/pipe.sh"
-if [ -e "EMACSENV_PLUGINS_DIR/pipe.sh" ]; then
-  ln -s "$plugin_file" $EMACSENV_PLUGINS_DIR/pipe.sh > /dev/null
+plugin_file="plugins/pipe.sh"
+if [ ! -e "EMACSENV_PLUGINS_DIR/pipe.sh" ]; then
+  ln -s "$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/$plugin_file" $EMACSENV_PLUGINS_DIR/pipe.sh > /dev/null
 fi
 
-. $plugin_file
+echo $plugin_file
