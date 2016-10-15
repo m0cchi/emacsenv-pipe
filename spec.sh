59c1a551302d961a2977e1433d9ccf3a19396d66
m0cchi <m0cchi@users.noreply.github.com>
2016-10-15 15:11:05 +0900

    add comment

--- spec.sh
+++ spec.sh
@@ -1,5 +1,7 @@
 plugin_file="plugins/pipe.sh"
+
 if [ ! -e "EMACSENV_PLUGINS_DIR/pipe.sh" ]; then
+  # automapping
   ln -s "$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/$plugin_file" $EMACSENV_PLUGINS_DIR/pipe.sh > /dev/null
 fi
 
